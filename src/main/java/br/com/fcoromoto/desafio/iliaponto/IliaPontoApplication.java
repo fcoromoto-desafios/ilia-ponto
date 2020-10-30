2fe96621222e56ec09cab0b5bd061dd84e1651e6
Francisco Coromoto <fcsouza1@stefanini.com>
2020-10-30 08:35:42 -0300

    first commit

---
+++ src/main/java/br/com/fcoromoto/desafio/iliaponto/IliaPontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fcoromoto.desafio.iliaponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliaPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IliaPontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fcoromoto/desafio/iliaponto/IliaPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fcoromoto.desafio.iliaponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IliaPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
